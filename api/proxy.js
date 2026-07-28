747f38f8f93ca24c489e93bf5c69dac6fef0b217
The Cook Rat <ijrambux@gmail.com>
2026-07-28 13:36:55 +0100

    Update proxy handler to use dynamic URL from request

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,14 @@
 // api/proxy.js
 export default async function handler(req, res) {
-    // الرابط الجديد الذي قمت بتحويله من بيانات Stalker (يعمل بنسبة 100%)
-    const M3U_URL = "http://4k.tvstb.me:80/get.php?username=1&2%20CB8F3108A1E319725C7955F70EE1394BF44D6E17ACA4F30CF947F1DFD7417E0C&password=1&2%20CB8F3108A1E319725C7955F70EE1394BF44D6E17ACA4F30CF947F1DFD7417E0C&type=m3u_plus";
+    // قراءة الرابط الذي يرسله المتصفح
+    const { url } = req.query;
+
+    if (!url) {
+        return res.status(400).send('يرجى إرسال رابط M3U في الرابط');
+    }
 
     try {
-        const response = await fetch(M3U_URL);
+        const response = await fetch(url);
         const data = await response.text();
         
         res.setHeader('Content-Type', 'text/plain');
